7681641a5b22cb10358e8116de949dd887fdbd81
JulioOEBoy <jcpcgame8@gmail.com>
2025-04-09 17:06:15 -0300

    Adiciona novos exercícios e atualizações

--- aula16b.py
+++ aula16b.py
@@ -12,8 +12,19 @@ for cont in range (0, len(lanche)):
     print(f'Eu vou comer {lanche[cont]}')
 print('Comi pra caramba')'''
 
-lanche = ('Hambúrguer','Suco', 'Pizza', 'Pudim')
+'''lanche = ('Hambúrguer','Suco', 'Pizza', 'Pudim')
 for pos, comida in enumerate(lanche):
     print(f'Eu vou comer {comida} na posição {pos+1}')
 
-print('Comida pra caramba!')
+print('Comida pra caramba!')'''
+
+# sorted organiza em ordem alfabetica
+
+'''lanche = ('Hambúrguer','Suco', 'Pizza', 'Pudim')
+print(sorted(lanche))'''
+
+a = (2,5,4)
+b = (5,8,1,2)
+c = a + b
+print(c.count(5))
+print(c.index(8))
