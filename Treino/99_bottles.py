36efb2156ab451de385e527ed4576b4e660c190f
JulioOEBoy <jcpcgame8@gmail.com>
2025-04-18 14:32:49 -0300

    Adiciona novos exercícios traduzidos para português

---
+++ Treino/99_bottles.py
@@ -0,0 +1,5 @@
+for garrafas in range(99, 0, -1):
+    print(f"{garrafas} garrafas de cerveja na parede")
+    print(f"{garrafas} garrafas de cerveja")
+    print("Pegue uma, passe adiante")
+    print(f"{garrafas - 1} garrafas de cerveja na parede\n")
